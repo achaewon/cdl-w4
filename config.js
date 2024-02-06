d771e6fe786002226a82b517c7c7cd483c230aca
achaewon <159125687+achaewon@users.noreply.github.com>
2024-02-06 17:36:24 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,112 @@
+var config = {
+    style: 'mapbox://styles/mapbox/dark-v11',
+    accessToken: 'pk.eyJ1IjoiYWhuY2hhZXdvbiIsImEiOiJjbHMwYmt0c3EwMGltMmxteHo3bXFhNmY0In0.ojMMt42AeFrqp1hbjHz_tQ',
+    showMarkers: true,
+    markerColor: '#3FB1CE',
+    //projection: 'equirectangular',
+    //Read more about available projections here
+    //https://docs.mapbox.com/mapbox-gl-js/example/projections/
+    inset: true,
+    theme: 'dark',
+    use3dTerrain: false, //set true for enabling 3D maps.
+    auto: false,
+    title: 'My commute today',
+    subtitle: 'Very personal map of Jan 31 2024',
+    byline: 'By Chaewon Ahn',
+    footer: 'Source: source citations, etc. <br> Created using <a href="https://github.com/mapbox/storytelling" target="_blank">Mapbox Storytelling</a> template.',
+    chapters: [
+        {
+            id: 'slug-style-id',
+            alignment: 'left',
+            hidden: false,
+            title: 'Home Location',
+            image: './path/to/image/KV.KV.jpeg',
+            description: 'Home is in faculty housing on NUS campus, called Kent Vale.',
+            location: {
+                center: [103.76906, 1.30212],
+                zoom: 18,
+                pitch: 56,
+                bearing: -16.80
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 1,
+                //     duration: 5000
+                // }
+            ],
+            onChapterExit: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 0
+                // }
+            ]
+        },
+        {
+            id: 'second-identifier',
+            alignment: 'right',
+            hidden: false,
+            title: 'Classroom EA 06-06',
+            image: './image/source.png',
+            description: 'The classroom t\for the Community Data Lab Course.',
+            location: {
+                center: [103.77044, 1.30015],
+                zoom: 18,
+                pitch: 61,
+                bearing: 24,
+                // flyTo additional controls-
+                // These options control the flight curve, making it move
+                // slowly and zoom out almost completely before starting
+                // to pan.
+                //speed: 2, // make the flying slow
+                //curve: 1, // change the speed at which it zooms out
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: true,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'third-identifier',
+            alignment: 'left',
+            hidden: false,
+            title: 'Third Title',
+            image: './path/to/image/source.png',
+            description: 'Copy these sections to add to your story.',
+            location: {
+                center: [6.15116, 46.20595],
+                zoom: 12.52,
+                pitch: 8.01,
+                bearing: 0.00
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'fourth-chapter',
+            alignment: 'fully',
+            hidden: false,
+            title: 'Third Title',
+            image: './path/to/image/source.png',
+            description: 'Copy these sections to add to your story.',
+            location: {
+                center: [-58.54195, -34.71600],
+                zoom: 4,
+                pitch: 0,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        }
+    ]
+};
